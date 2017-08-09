977229ae98f06ad5abe57516536a9477c5c311c7
sjones6 <jones.spencera@gmail.com>
2017-08-08 23:48:31 -0400

    Commands for testing

---
+++ bin/commands/integration-suite/data.js
@@ -0,0 +1,99 @@
+module.exports = {
+
+    // Cars
+    civic: {
+        key: 'civic',
+        val: {
+            make: 'honda',
+            luxury: false,
+            year: 2016,
+            engine: 'v8'
+        }
+    },
+    z4: {
+        key: 'z4',
+        val: {
+            make: 'bmw',
+            luxury: true,
+            year: 2016,
+            engine: null
+        }
+    },
+    focus: {
+        key: 'focus', 
+        val: {
+            make: 'ford',
+            luxury: true,
+            year: 2016,
+            engine: null
+        }
+    },
+    f150: {
+        key: 'f-150', 
+        val: {
+            make: 'ford',
+            luxury: true,
+            year: 2016,
+            engine: null
+        }
+    },
+    passat: {
+        key: 'passat',
+        val: {
+            make: 'vw',
+            luxury: false,
+            year: 2016,
+            engine: 'diesel'
+        }
+    },
+
+    // Makers
+    honda: {
+        key: 'honda',
+        val: {
+            name: 'honda',
+            country: 'japan'
+        }
+    },
+    ford: {
+        key: 'ford',
+        val: {
+            name: 'ford',
+            country: 'usa'
+        }
+    },
+    bmw: {
+        key: 'bmw',
+        val: {
+            name: 'bmw',
+            country: 'germany'
+        }
+    },
+    vw: {
+        key: 'vw',
+        val: {
+            name: 'vw',
+            country: 'germany'
+        }
+    },
+
+    // Countries
+    germany: {
+        key: 'germany',
+        val: {
+            continent: 'europe'
+        }
+    },
+    japan: {
+        key: 'japan',
+        val: {
+            continent: 'asia'
+        }
+    },
+    usa: {
+        key: 'usa',
+        val: {
+            continent: 'north_america'
+        }
+    }
+};
---
+++ bin/commands/integration-suite/index.js
@@ -0,0 +1,82 @@
+module.exports = function(finished, Adapter, opt) {
+
+    const path = require('path');
+    const Mocha = require('mocha');
+    const fs = require('fs');
+
+    // Essential opt setup
+    opt = opt || {};
+    opt.file = false;
+    
+    // Set a few globals (can this be done another way??)
+    global.Gun = require('gun/gun');
+    Adapter.bootstrap(global.Gun);
+    global.Adapter = Adapter;
+    global.opt = opt;
+    global.data = require('./data');
+
+    // Ensure unique identity for this test run.
+    global.keyBase = Date.now() + "";    
+    global.getKey = (suffix) => {
+        return `${keyBase}_${suffix}`;
+    }
+
+    // An easy way to re-create gun as needed
+    global.getGun = function() {
+        return new Gun(opt);
+    }
+
+
+    // Log warning
+    console.info("You are about to run an integration test suite against your adapter. If this is linked to a storage layer, it WILL write records to that storage layer.");
+
+
+    // Instantiate a Mocha instance.
+    var mocha = new Mocha();
+
+    let testDir = path.join(__dirname, 'suite/');
+
+    // Add each .js file to the mocha instance
+    fs.readdirSync(testDir).filter(function(file){
+        // Only keep the .js files
+        return file.substr(-3) === '.js';
+
+    }).forEach(function(file) {
+        mocha.addFile(
+            path.join(testDir, file)
+        );
+    });
+
+    // Run the tests.
+    let runner = mocha.run();
+
+    /*
+    *   - `start`  execution started
+    *   - `end`  execution complete
+    *   - `suite`  (suite) test suite execution started
+    *   - `suite end`  (suite) all tests (and sub-suites) have finished
+    *   - `test`  (test) test execution started
+    *   - `test end`  (test) test completed
+    *   - `hook`  (hook) hook execution started
+    *   - `hook end`  (hook) hook complete
+    *   - `pass`  (test) test passed
+    *   - `fail`  (test, err) test failed
+    *   - `pending`  (test) test pending
+    */
+    runner.on('end', () => {
+        finished();
+    });
+    // .on('start', () => {
+    //     console.log('TESTING STARTED');
+    // })
+    
+    // .on('pass', test => {
+    //     console.log('.');
+    // })
+    // .on('fail', (test) => {
+    //     console.log('F');
+    // })
+    // .on('suite end', suite => {
+    //     console.log("INTEGRATION SUITE FINISHED");
+    // });
+}
